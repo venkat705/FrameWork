3f8e2991cce906ebfc8fb30c51a3d9948cd873a0
venkat705 <dirisala536@gmail.com>
2021-03-05 12:20:11 +0530

    code pushed

---
+++ SeleniumProj/src/main/java/pom/project/SeleniumProj/App.java
@@ -0,0 +1,13 @@
+package pom.project.SeleniumProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ SeleniumProj/src/main/java/testing/BrowserLaunch.java
@@ -0,0 +1,35 @@
+package testing;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class BrowserLaunch {
+	
+	@Test
+	
+	public void test()
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		RemoteWebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
+		driver.findElementByXPath("//input[@name='q']").sendKeys(Keys.SPACE );
+		List<WebElement> findElements = driver.findElements(By.xpath("//ul[@class='erkvQe']/li[@class='sbct']"));
+		for (WebElement webElement : findElements) {
+		 System.out.println(webElement.getText());
+
+			
+		}
+		
+	}
+
+}
